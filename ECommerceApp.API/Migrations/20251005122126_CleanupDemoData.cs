8e54bf8331f5d37bf0c49f597982228d702d998e
Bảo Trần <tmb14112004@gmail.com>
2025-10-05 19:27:41 +0700

    fix frontend

---
+++ ECommerceApp.API/Migrations/20251005122126_CleanupDemoData.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceApp.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanupDemoData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Delete all products with via.placeholder.com images (demo data)
+            migrationBuilder.Sql("DELETE FROM \"Products\" WHERE \"Image\" LIKE '%via.placeholder.com%'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
